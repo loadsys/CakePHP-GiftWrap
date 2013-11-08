160b281206d4b8377f0c29f2293203b69dd18abd
Joey Trapp <joey@loadsys.com>
2013-11-08 16:30:34 -0600

    Added DecoratorIterator that is the same as PresenterIterator

---
+++ Lib/DecoratorListIterator.php
@@ -0,0 +1,47 @@
+<?php
+
+class DecoratorListIterator extends ArrayIterator {
+	protected $_class;
+	protected $_extra = array();
+	protected $_options;
+	protected $_controller = null;
+	protected $_cache = array();
+
+	public function __construct(
+		$array,
+		$class,
+		$extra = array(),
+		$options = array(),
+		$controller = null
+	) {
+		parent::__construct($array);
+		$this->_class = $class;
+		$this->_extra = $extra;
+		$this->_options = $options;
+		$this->_controller = $controller;
+	}
+
+	public function current() {
+		return $this->cache($this->key(), parent::current());
+	}
+
+	public function offsetGet($index) {
+		return $this->cache($index, parent::offsetGet($index));
+	}
+
+	protected function cache($index, $value) {
+		if (isset($this->_cache[$index])) {
+			$val = $this->_cache[$index];
+		} else {
+			$val = $this->_cache[$index] = $this->wrap($value);
+		}
+		return $val;
+	}
+
+	protected function wrap($value) {
+		$options = $this->_options;
+		$presenter = new $this->_class($this->_extra, $options, $this->_controller);
+		$presenter->setContext($value);
+		return $presenter;
+	}
+}
---
+++ Test/Case/Lib/DecoratorListIteratorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+App::uses('Presenter', 'CakePHP-GiftWrap.Presenter');
+App::uses('DecoratorListIterator', 'CakePHP-GiftWrap.Lib');
+
+class DecoratorListIteratorTest extends CakeTestCase {
+	public function testWrapsEachItemInArrayInAPresenter() {
+		$data = array('one', 'two', 'three');
+		$list = new DecoratorListIterator($data, 'Presenter');
+		$this->assertEquals('one', $list[0]->model);
+		$this->assertEquals('two', $list[1]->model);
+		$this->assertEquals('three', $list[2]->model);
+	}
+
+	public function testPresenterObjectsAreCachedForReuse() {
+		$data = array('one');
+		$list = new DecoratorListIterator($data, 'Presenter');
+		$one = $list[0];
+		$one->key = 'value';
+		$this->assertEquals('value', $list[0]->key);
+	}
+}
